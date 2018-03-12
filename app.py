74675dfd15e5cd398e444f80bd22397d85dfc3fb
Eva Lieu <itsevalieu@gmail.com>
2018-03-11 23:10:22 -0700

    Adding initial files for directory

---
+++ app.py
@@ -0,0 +1,6 @@
+def knearestneighbor(trainfile, testfile, outputfile, k):
+    print(trainfile, testfile, outputfile, k)
+    confusionmatrix = ""
+    return confusionmatrix
+
+# find_k('iristrain.csv', 'iristest.csv', '3)
